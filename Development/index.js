5197b3b351f5db4d6863382f29ca358fc6fffb12
Jordan Nadal <nadal_jordan@yahoo.com>
2022-07-26 07:29:08 -0500

    added writeHtml function

--- Development/index.js
+++ Development/index.js
@@ -1,7 +1,13 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
 
 const saveData = [];
 
+const manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const employee = require("./lib/Employee");
 
 function callManager() {
   inquirer
@@ -9,17 +15,17 @@ function callManager() {
       {
         type: `input`,
         message: `What is your Managers name?`,
-        name: `Manager`,
+        name: `Name`,
       },
       {
         type: `input`,
         message: `what is the managers ID#`,
-        name: "managerId",
+        name: "Id",
       },
       {
         type: `input`,
         message: `what is the managers Email?`,
-        name: "managerEmail",
+        name: "Email",
       },
       {
         type: `input`,
@@ -29,6 +35,7 @@ function callManager() {
     ])
     .then((answers) => {
       console.log(answers.officeNum);
+      answers.title = "manager";
       saveData.push(answers);
 
       AnyOthers();
@@ -41,26 +48,27 @@ function callEngineer() {
       {
         type: `input`,
         message: `what is the Engineers name?`,
-        name: "EngineerName",
+        name: "Name",
       },
       {
         type: `input`,
         message: `what is their ID#?`,
-        name: `EngId`,
+        name: `Id`,
       },
       {
         type: `input`,
         message: `what is their Email?`,
-        name: `EngEmail`,
+        name: `Email`,
       },
       {
         type: `input`,
         message: `what is their github username?`,
-        name: "EngGithub",
+        name: "Github",
       },
     ])
     .then((answers) => {
       console.log(answers);
+      answers.title = "Engineer";
       saveData.push(answers);
 
       AnyOthers();
@@ -73,26 +81,27 @@ function callIntern() {
       {
         type: `input`,
         message: `what is the interns name?`,
-        name: "InternName",
+        name: "Name",
       },
       {
         type: `input`,
         message: `what is the interns ID#?`,
-        name: `InternID`,
+        name: `Id`,
       },
       {
         type: `input`,
         message: `what is the interns Email?`,
-        name: `InternEmail`,
+        name: `Email`,
       },
       {
         Type: `input`,
         message: `what school does the intern attend?`,
-        name: `InternSchool`,
+        name: `School`,
       },
     ])
     .then((answers) => {
       console.log(answers);
+      answers.title = "intern";
       saveData.push(answers);
 
       AnyOthers();
@@ -111,20 +120,75 @@ function AnyOthers() {
     ])
     .then((answers) => {
       console.log(answers);
-      if(answers.employeesQuestion === 'Manager'){
+      if (answers.employeesQuestion === "Manager") {
         callManager();
       }
-      if(answers.employeesQuestion === 'Engineer'){
+      if (answers.employeesQuestion === "Engineer") {
         callEngineer();
       }
-      if(answers.employeesQuestion === 'Intern'){
+      if (answers.employeesQuestion === "Intern") {
         callIntern();
       }
-      if(answers.employeesQuestion === 'noone'){
+      if (answers.employeesQuestion === "noone") {
         console.log(saveData);
+        writeHtml(saveData);
       }
-
     });
 }
 
 callManager();
+
+function writeHtml(information) {
+  console.log(information, "info");
+  let team = "";
+  information.forEach((item) => {
+    team += `<div class="card" style="width: 18rem">
+    <div class="list-group list-group-flush">
+      <h1 class="name">${item.Name}</h1>
+      <h2 class="title">${item.title}</h2>
+      <a class="id1">ID:${item.Id}</a>
+      <href>Email:${item.Email}</href>
+      ${item.GitHub ? `<href>GitHub:${item.Github}</href>` : ""}
+      ${item.school ? `<li>school:${item.school}</li>` : ""}
+      ${item.officeNum ? `<li>office#:${item.officeNum}</li>` : ""}
+      
+    
+    </div>`;
+  });
+
+  let html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="./assets/style.css" />
+      <title>Meet The Team</title>
+      <!-- <link rel="stylesheet" href="jass.css" /> -->
+    </head>
+    <body>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="header">
+          <h1 class="display-4">Meet The Team</h1>
+        </div>
+      </div>
+      ${team}
+    </body>
+  
+    <script src="index.js"></script>`;
+
+  fs.writeFile(__dirname + "/../public/index.html", html, (err) => {
+    if (err) {
+      console.error(err, "HTMl not generated");
+      return;
+    }
+    console.log("HTML succesfully written");
+    // file written successfully
+  });
+}
